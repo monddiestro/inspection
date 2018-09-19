8a5934540da01e95b8dfd53f845c1973cbf0cfdf
zonel picadizo <zonel.picadizo@carmudi.com.ph>
2018-09-19 13:28:52 +0800

    design

--- application/views/inspection.php
+++ application/views/inspection.php
@@ -1,32 +1,41 @@
 <div class="container">
-  <div class="">
-    <div class="row">
-      <div class="col-sm-12 col-md-12 col-lg-12">
-        <div class="table-responsive">
-          <table class="table table-bordered">
-            <thead>
-              <th>Listing ID</th>
-              <th>URL</th>
-              <th>Dealer Name</th>
-              <th>Unit</th>
-              <th>Date Created</th>
-            </thead>
-            <tbody>
-              <?php foreach ($list as $d): ?>
-                <tr>
-                  <td>
-                    <a href="<?php echo base_url('inspection/config?id='.$d->inspected_id."&listing_id=".$d->listing_id) ?>">
-                      <?php echo $d->listing_id ?>
-                    </a>
-                  </td>
-                  <td><?php echo $d->listing_uri ?></td>
-                  <td><?php echo $d->dealer_name ?></td>
-                  <td><?php echo $d->unit ?></td>
-                  <td><?php echo $d->date_created ?></td>
-                </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
+  <div class="page-mtop">
+    <div class="card">
+      <div class="table-header">
+        <div class="row">
+          <div class="col-md-offset-8 col-md-4">
+            <div class="table-filter text-right" id="table_filter">
+              <label><input type="search" class="form-control" id="" placeholder="Search"  ></labe;>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="flip-scroll">
+              <table class="table">
+                <thead>
+                  <th>Listing ID</th>
+                  <th>URL</th>
+                  <th>Dealer Name</th>
+                  <th>Unit</th>
+                  <th>Date Created</th>
+                </thead>
+                <tbody>
+                  <?php foreach ($list as $d): ?>
+                    <tr>
+                      <td>
+                        <a href="<?php echo base_url('inspection/config?id='.$d->inspected_id."&listing_id=".$d->listing_id) ?>">
+                          <?php echo $d->listing_id ?>
+                        </a>
+                      </td>
+                      <td><?php echo $d->listing_uri ?></td>
+                      <td><?php echo $d->dealer_name ?></td>
+                      <td><?php echo $d->unit ?></td>
+                      <td><?php echo $d->date_created ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- application/views/nav.php
+++ application/views/nav.php
@@ -1,12 +1,12 @@
 <!-- navigation  -->
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-8" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar top-bar"></span>
+        <span class="icon-bar middle-bar"></span>
+        <span class="icon-bar bottom-bar"></span>
       </button>
       <a class="navbar-brand" href="<?php echo base_url(); ?>">
         <img src="https://www.carmudi.com.ph/images/relaunch/carmudi-logo-PH.svg" alt="Carmudi Philippines Inc.">
@@ -17,7 +17,7 @@
         <li class="<?php echo ($menu == 'inspected') ? 'active' : '' ?>"><a href="<?php echo base_url() ?>">Inspected</a></li>
         <li class="<?php echo ($menu == 'request') ? 'active' : '' ?>"><a href="<?php echo base_url('inspection/request') ?>">Request</a></li>
         <li class="<?php echo ($menu == 'access') ? 'active' : '' ?>"><a href="<?php echo base_url('inspection/access') ?>">Access</a></li>
-        <li><a href="#"><i class="material-icons">exit_to_app</i></a></li>
+        <li> <a href="#"><i class="material-icons">exit_to_app</i></a></li>
       </ul>
     </div>
   </div>
